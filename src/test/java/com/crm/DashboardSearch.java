045fb5a6e3b2912eda9c9e307e1a2e84bee703cf
Cristea3051 <cristeavictor4@gmail.com>
2024-06-11 11:21:02 +0300

    Am adaugat dashboard search pentru crm

---
+++ src/test/java/com/crm/DashboardSearch.java
@@ -0,0 +1,62 @@
+package com.crm;
+
+import org.testng.Reporter;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import java.util.List;
+
+import com.resources.CredentialsProvider;
+import com.utilities.Login;
+
+public class DashboardSearch {
+    private WebDriver driver;
+    private Login login;
+
+    @BeforeMethod
+    public void setUp() {
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        login = new Login(driver); // Inițializează obiectul Login
+    }
+
+    @Test(dataProvider = "credentials", dataProviderClass = CredentialsProvider.class)
+    public void signIn(String username, String password) {
+        login.performLogin(username, password);
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("page-header-user-dropdown"))).click(); 
+        WebElement element = wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("div.bg-primary-dark p.font-size-sm")));
+        String role = element.getText();
+
+        Reporter.log("Utilizator "  + username + " - " + role + " s-a logat logat");
+ // Definiți și inițializați wait
+    wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("input.form-control:nth-child(1)")));
+
+    List<WebElement> spanElements = driver.findElements(By.cssSelector("div.block-content span.filter.drop-shadow.small"));
+
+    // Se iterează prin lista de elemente și se extrage textul din fiecare element
+    for (WebElement spanElement : spanElements) {
+        String card = spanElement.getText();
+        Reporter.log("A fost localizat: "  + card);
+    }
+
+    
+    }
+
+    @AfterMethod
+    public void tearDown() {
+        // Închide browser-ul
+        if (driver != null) {
+            driver.quit();
+        }
+    }
+}
---
+++ src/test/java/com/utilities/Login.java
@@ -0,0 +1,31 @@
+package com.utilities;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import java.time.Duration;
+
+public class Login {
+    private WebDriver driver;
+    private WebDriverWait wait;
+
+    public Login(WebDriver driver) {
+        this.driver = driver;
+        wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+    }
+
+    public void performLogin(String username, String password) {
+        driver.get("http://crm-dash/login");
+
+        // Localizează și completează câmpurile de logare
+        WebElement usernameField = wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("login-username")));
+        WebElement passwordField = driver.findElement(By.id("login-password"));
+        WebElement loginButton = driver.findElement(By.tagName("button"));
+
+        usernameField.sendKeys(username);
+        passwordField.sendKeys(password);
+        loginButton.click();
+    }
+}
