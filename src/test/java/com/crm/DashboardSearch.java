10739b7f4920c667795395475114dc7a37c60036
Cristea3051 <cristeavictor4@gmail.com>
2024-06-11 13:43:39 +0300

    niste chestii la DashboardSearch

--- src/test/java/com/crm/DashboardSearch.java
+++ src/test/java/com/crm/DashboardSearch.java
@@ -32,15 +32,24 @@ public class DashboardSearch {
     @Test(dataProvider = "credentials", dataProviderClass = CredentialsProvider.class)
     public void signIn(String username, String password) {
         login.performLogin(username, password);
+        // Initializez elementul wait
         WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+
+        // Localizez si aplic click() pe butonul user dropdown din header
         wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("page-header-user-dropdown"))).click(); 
+
+        // Extrag textul cu rolul pe care
         WebElement element = wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("div.bg-primary-dark p.font-size-sm")));
         String role = element.getText();
 
+    
+
         Reporter.log("Utilizator "  + username + " - " + role + " s-a logat logat");
- // Definiți și inițializați wait
+
+    //   Localizez elementul cu ajutorul lui wait
     wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("input.form-control:nth-child(1)")));
 
+    // Afisez lista de elemente cu acelasi css selector 
     List<WebElement> spanElements = driver.findElements(By.cssSelector("div.block-content span.filter.drop-shadow.small"));
 
     // Se iterează prin lista de elemente și se extrage textul din fiecare element
