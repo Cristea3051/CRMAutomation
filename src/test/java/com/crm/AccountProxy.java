6f2bac081b3c6e81294d9521c6cfa2961fc84480
Cristea3051 <cristeavictor4@gmail.com>
2024-06-12 17:06:36 +0300

    am adaugat update dar am problema cu metoda

--- src/test/java/com/crm/AccountProxy.java
+++ src/test/java/com/crm/AccountProxy.java
@@ -100,12 +100,73 @@ public class AccountProxy {
     }
 
     // Update Proxy
+
+
+ WebElement proxyCheckbox = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("/html/body/div[2]/main/div[2]/div/div/div/div[2]/div[2]/div/div/div/div[3]/div[2]/table/tbody/tr[1]/td[1]/div/input")));
+proxyCheckbox.click(); 
+
+ wait.until(ExpectedConditions.elementToBeClickable(By.id("edit-dt-row"))).click();
+
+ WebElement editName = wait.until(ExpectedConditions.elementToBeClickable(By.id("edit_name")));
+
+ editName.clear();
+ editName.sendKeys("101.232.32.25:2255");
+
+ WebElement editUserName = wait.until(ExpectedConditions.elementToBeClickable(By.id("edit_username")));
+
+ editUserName.clear();
+ editUserName.sendKeys("UpdatedUsername");
+
+ WebElement editPassword = wait.until(ExpectedConditions.elementToBeClickable(By.id("edit_password")));
+editPassword.clear();
+ editPassword.sendKeys("UpdatedPass22##");
+
+
+wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//*[@id=\"edit-accounts-proxy-modal\"]/div/div/div/div/div/div/div[2]/form/div/div/div[4]/select"))).click();
+
+driver.switchTo().activeElement().sendKeys(Keys.ARROW_DOWN);
+driver.switchTo().activeElement().sendKeys(Keys.ENTER);
+
+WebElement sourceName = driver.findElement(By.cssSelector("input[data-modal-field-id='edit_source']"));
+try {
+    Thread.sleep(4000);
+} catch (InterruptedException e) {
+    e.printStackTrace();
+}
+sourceName.clear();
+sourceName.sendKeys("ip");
+try {
+    Thread.sleep(2000);
+} catch (InterruptedException e) {
+    e.printStackTrace();
+}
+driver.switchTo().activeElement().sendKeys(Keys.ARROW_DOWN);
+driver.switchTo().activeElement().sendKeys(Keys.ENTER);
+
+
+WebElement updateProxy = wait.until(ExpectedConditions.elementToBeClickable(By.id("edit-accounts-proxy-button")));
+((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView(true);", updateProxy);
+updateProxy.click();
+
+try {
+Thread.sleep(4000);
+} catch (InterruptedException e) {
+e.printStackTrace();
+}
+
+// Iterează primele 5 elemente sau mai puține, dacă lista are mai puțin de 5 elemente
+for (int i = 0; i < count; i++) {
+WebElement cell = cells.get(i);
+String text = cell.getText();
+Reporter.log("A fost eidtat proxyul: " + text);
+}
+
     // Create tabble settings 
     // Delete table settings 
     // Export csv
         // Delete proxy
-        WebElement proxyCheckbox = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("/html/body/div[2]/main/div[2]/div/div/div/div[2]/div[2]/div/div/div/div[3]/div[2]/table/tbody/tr/td[1]/div/input")));
-        proxyCheckbox.click();
+        WebElement selectCheckbox = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("/html/body/div[2]/main/div[2]/div/div/div/div[2]/div[2]/div/div/div/div[3]/div[2]/table/tbody/tr/td[1]/div/input")));
+        selectCheckbox.click();
         
         try {
             // Așteaptă un interval pentru a fi sigur că se face clic pe proxy
