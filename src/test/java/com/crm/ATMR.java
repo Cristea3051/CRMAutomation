4a6c87a0fc72a10474a53e7ff5e5a23bd2d648df
Cristea3051 <cristeavictor4@gmail.com>
2024-08-12 16:43:58 +0300

    sdsadsa

--- src/test/java/com/crm/ATMR.java
+++ src/test/java/com/crm/ATMR.java
@@ -4,8 +4,6 @@ import java.time.Duration;
 import java.util.List;
 
 import org.openqa.selenium.By;
-import org.openqa.selenium.JavascriptExecutor;
-import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
