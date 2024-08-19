e0549e93bcdf1bb2c9afa71ca60a3c9eaf1cb32a
Cristea3051 <cristeavictor4@gmail.com>
2024-08-19 15:19:09 +0300

    dsadsa

--- src/test/java/com/Base/BaseTest.java
+++ src/test/java/com/Base/BaseTest.java
@@ -2,7 +2,6 @@ package com.Base;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 
