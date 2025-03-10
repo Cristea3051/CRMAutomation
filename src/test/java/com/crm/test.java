31fe6f59a44a390802ef2b9fc421b2d6346e6a6a
Cristea3051 <cristeavictor4@gmail.com>
2025-03-10 14:55:31 +0200

    dadsadsa

--- src/test/java/com/crm/test.java
+++ src/test/java/com/crm/test.java
@@ -32,72 +32,30 @@ public class test extends BaseTest {
     }
 
     @Test(dataProvider = "MediaBuyerGlobalCredentials", dataProviderClass = CredentialsProvider.class)
-    public void signIn(String username, String password) throws InterruptedException {
+    public void signIn(String username, String password) {
         login.performLogin(username, password);
         Reporter.log("Utilizator " + username + " s-a logat");
 
         login.closeDebugBar();
 
-         driver.get("http://crm-dash/google-accounts");
-
-        String title = driver.getTitle();
-        Reporter.log("Utilizatorul a navigat cu succes la pagina - " + title);
+        super.driver.get("http://crm-dash/google-accounts-v2");
 
         Helpers.waitForSeconds(3);
+        List<WebElement> headers = driver.findElements(By.cssSelector(
+                "div[role='columnheader']"));
 
-        WebElement select = driver.findElement(By.name("google-accounts-list_length"));
-        Select rows = new Select(select);
-        rows.selectByIndex(0);
 
-        wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector("i.fa.fa-caret-down"))).click();
-
-        wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector(
-                "li.active:nth-child(13)"))).click();
-
-        Helpers.waitForSeconds(3);
-        
-        driver.findElement(By.cssSelector(".fa-table")).click();
-        Helpers.waitForSeconds(3);
-        
-        WebElement selectSettings = driver.findElement(By.cssSelector("select#swap-from.custom-select"));
-        
-        // Crează un obiect Select pentru a interacționa cu dropdown-ul
-        Select SettingsRows = new Select(selectSettings);
-        
-        Helpers.waitForSeconds(3);
-        List<WebElement> headers = driver.findElements(By.cssSelector(".dataTables_scrollHeadInner > table:nth-child(1) > thead:nth-child(1) > tr:nth-child(1) > th"));
-        List<WebElement> firstRow = driver.findElements(By.cssSelector("#google-accounts-list tbody tr:first-child td"));
-        
-        // Iterează prin fiecare coloană din tabel
-        for (int i = 0; i < headers.size(); i++) {
+        for (WebElement webElement : headers) {
             // Scroll până la elementul curent din header
-            ((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView(true);", headers.get(i));
-        
-            String header = headers.get(i).getText().trim();
-            String content = (i < firstRow.size()) ? firstRow.get(i).getText().trim() : "";
-        
-            if (!header.isEmpty()) {
-                // Iterează prin opțiunile din dropdown
-                boolean optionFound = false;
-                for (WebElement option : SettingsRows.getOptions()) {
-                    // Compară textul header + content cu opțiunile din dropdown
-                    if (option.getText().contains(header) && option.getText().contains(content)) {
-                        Reporter.log(header + " -> " + content + " -> Found in dropdown: " + option.getText());
-                        optionFound = true;
-                        break;  // Ieși din buclă dacă găsești o potrivire
-                    }
-                }
-        
-                // Dacă nu s-a găsit opțiunea, scrie un mesaj că nu a fost găsită
-                if (!optionFound) {
-                    Reporter.log(header + " -> " + content + " -> Not found in dropdown");
-                }
-            } else {
-                Reporter.log("Header is empty for index " + i);
-            }
-        }        
-        
-        driver.quit();
+            ((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView(true);", webElement);
 
+            String header = webElement.getText().trim();
+
+            if (!header.isEmpty()) {
+                Reporter.log(header);
+            } else {
+                Reporter.log("Header is empty for index ");
+            }
+        }
     }
 }
