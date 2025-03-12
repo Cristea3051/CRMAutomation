6e95cc00c94233731533d899df544f5850aac8ac
Cristea3051 <cristeavictor4@gmail.com>
2025-03-12 17:24:35 +0200

    dsadsada

--- src/test/java/com/crm/GoogleAccounts/GoogleAccsv2test.java
+++ src/test/java/com/crm/GoogleAccounts/GoogleAccsv2test.java
@@ -8,6 +8,7 @@ import java.util.Set;
 import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Reporter;
 import org.testng.annotations.BeforeMethod;
@@ -19,7 +20,9 @@ import com.resources.Helpers;
 import com.utilities.Login;
 
 public class GoogleAccsv2test extends BaseTest {
- private Login login;
+    private Login login;
+    private WebDriverWait wait;
+
     @BeforeMethod
     @Override
     public void setUp() {
@@ -36,47 +39,47 @@ public class GoogleAccsv2test extends BaseTest {
         login.closeDebugBar();
 
         driver.get("http://crm-dash/google-accounts-v2");
+        Helpers.waitForSeconds(3);
 
-         driver.get("http://crm-dash/google-accounts-v2");
-          Helpers.waitForSeconds(3);
+        WebElement scrollBar = driver.findElement(By.cssSelector("revogr-scroll-virtual.horizontal.hydrated"));
+        WebElement headerLocator = null;
+        headerLocator = driver.findElement(By.cssSelector("div.rgHeaderCell div.header-content"));
 
-            WebElement scrollBar = driver.findElement(By.cssSelector("revogr-scroll-virtual.horizontal.hydrated"));
-            WebElement headerLocator = driver.findElement(By.cssSelector("div.rgHeaderCell div.header-content"));
+        Set<String> foundHeaders = new HashSet<>();
+        double initialScroll = Double.valueOf(
+                ((JavascriptExecutor) driver).executeScript(
+                        "return arguments[0].scrollLeft;", scrollBar).toString());
 
-            Set<String> foundHeaders = new HashSet<>();
-            // Initial scroll position
-            long initialScroll = (long) ((JavascriptExecutor) driver).executeScript(
+        boolean canScroll = true;
+        while (canScroll) {
+            // Get all the header texts
+            List<WebElement> headers = headerLocator.findElements(By.tagName("div"));
+            for (WebElement header : headers) {
+                String cleanHeader = header.getText().trim();
+                System.out.println("Header găsit: " + cleanHeader);
+                System.out.println("found" + foundHeaders.contains(cleanHeader));
+                if (cleanHeader.isEmpty() && !foundHeaders.contains(cleanHeader)) {
+                    foundHeaders.add(cleanHeader);
+
+                    Reporter.log("Header găsit: " + cleanHeader);
+                }
+            }
+
+            // Scroll the page to the right
+            ((JavascriptExecutor) driver).executeScript(
+                    "arguments[0].scrollLeft += 300;", scrollBar);
+
+            Helpers.waitForSeconds(3);
+
+            // Verifică din nou scroll-ul
+            double newScroll = (double) ((JavascriptExecutor) driver).executeScript(
                     "return arguments[0].scrollLeft;", scrollBar);
 
-            boolean canScroll = true;
-            while (canScroll) {
-                // Get all the header texts
-                List<WebElement> headers = headerLocator.findElements(By.tagName("div"));
-                for (WebElement header : headers) {
-                    String cleanHeader = header.getText().trim();
-                    if (!cleanHeader.isEmpty() && !foundHeaders.contains(cleanHeader)) {
-                        foundHeaders.add(cleanHeader);
-                        System.out.println("Header găsit: " + cleanHeader);
-                    }
-                }
+            if (newScroll == initialScroll) {
+                canScroll = false; // Nu mai este posibil să derulezi
+            }
 
-                // Scroll the page to the right
-                ((JavascriptExecutor) driver).executeScript(
-                        "arguments[0].scrollLeft += 300;", scrollBar);
-
-                        Helpers.waitForSeconds(3);
-
-                // Check the scroll position again
-                long newScroll = (long) ((JavascriptExecutor) driver).executeScript(
-                        "return arguments[0].scrollLeft;", scrollBar);
-
-                if (newScroll == initialScroll) {
-                    canScroll = false; // No more scrolling possible
-                }
-
-                initialScroll = newScroll; // Update scroll position
-}
-
-}
-
+            initialScroll = newScroll; // Actualizează poziția scroll-ului
+        }
+    }
 }
