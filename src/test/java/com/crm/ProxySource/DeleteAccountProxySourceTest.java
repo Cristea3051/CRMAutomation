9c86273f641a86a96b3bdf572c66559141a619cc
Cristea3051 <cristeavictor4@gmail.com>
2025-03-14 14:47:37 +0200

    dasdsadsads

---
+++ src/test/java/com/crm/ProxySource/DeleteAccountProxySourceTest.java
@@ -0,0 +1,84 @@
+package com.crm.ProxySource;
+
+import com.Base.BaseTest;
+import com.resources.CredentialsProvider;
+import com.resources.Helpers;
+import com.utilities.Login;
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
+import org.testng.Reporter;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import java.time.Duration;
+import java.util.List;
+
+public class DeleteAccountProxySourceTest extends BaseTest {
+
+    private Login login;
+    private WebDriverWait wait;
+
+    @BeforeMethod
+    @Override
+    public void setUp() {
+        super.setUp();
+        login = new Login(driver);
+        wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+    }
+
+    @Test(dataProvider = "GlobalCred", dataProviderClass = CredentialsProvider.class)
+    public void signIn(String username, String password) {
+        login.performLogin(username, password);
+        Reporter.log("Utilizator " + username + " s-a logat");
+
+        login.closeDebugBar();
+
+        driver.get("http://crm-dash/accounts-proxy-source");
+
+        String title = driver.getTitle();
+        Reporter.log("Utilizatorul a navigat cu succes la pagina - " + title);
+
+        Helpers.waitForSeconds(3);
+
+        wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector("input.form-control[type='search']")))
+                .sendKeys("AutomatedAccountProxySource");
+        Helpers.waitForSeconds(3);
+        wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector("#account-proxy-sources tbody tr:first-child td:first-child"))).click();
+        Helpers.waitForSeconds(3);
+        wait.until(ExpectedConditions.elementToBeClickable(By.id("delete-dt-row"))).click();
+        Helpers.waitForSeconds(3);
+        wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector("button.swal2-confirm"))).click();
+        Helpers.waitForSeconds(3);
+
+        boolean isButtonGone = wait.until(ExpectedConditions.invisibilityOfElementLocated(By.cssSelector("button.swal2-confirm")));
+
+        Assert.assertTrue(isButtonGone);
+
+        Reporter.log("AutomatedAccountProxySource a fost sters cu success!", true);
+
+        List<WebElement> headers = driver.findElements(By.cssSelector(
+                ".dataTables_scrollHeadInner > table:nth-child(1) > thead:nth-child(1) > tr:nth-child(1) > th"));
+        List<WebElement> firstRow = driver
+                .findElements(By.cssSelector("#account-proxy-sources tbody tr:first-child td"));
+
+        for (int i = 0; i < headers.size(); i++) {
+            ((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView(true);", headers.get(i));
+
+            String header = headers.get(i).getText().trim();
+            String content = (i < firstRow.size()) ? firstRow.get(i).getText().trim() : "";
+
+            if (!header.isEmpty()) {
+                Reporter.log(header + " -> " + content);
+            } else {
+                Reporter.log("Header is empty for index ");
+            }
+        }
+    }
+}
+
+
+
