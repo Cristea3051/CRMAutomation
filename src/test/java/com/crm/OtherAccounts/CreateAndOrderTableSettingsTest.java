b59165603f86ade7d857ef85b604f513f409f9a3
Cristea3051 <cristeavictor4@gmail.com>
2025-03-14 12:47:00 +0200

    dsadsadsa

---
+++ src/test/java/com/crm/OtherAccounts/CreateAndOrderTableSettingsTest.java
@@ -0,0 +1,107 @@
+package com.crm.OtherAccounts;
+
+import com.Base.BaseTest;
+import com.resources.CredentialsProvider;
+import com.resources.Helpers;
+import com.resources.configfiles.SettingsHelper;
+import com.utilities.Login;
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Reporter;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import java.time.Duration;
+import java.util.List;
+
+public class CreateAndOrderTableSettingsTest extends BaseTest {
+    private Login login;
+    private WebDriverWait wait;
+
+    @BeforeMethod
+    @Override
+    public void setUp() {
+        super.setUp();
+        login = new Login(driver);
+        wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+    }
+
+    @Test(dataProvider = "GlobalCred", dataProviderClass = CredentialsProvider.class)
+    public void signIn(String username, String password) {
+        login.performLogin(username, password);
+        Reporter.log("Utilizator " + username + " s-a logat");
+
+        login.closeDebugBar();
+
+        driver.get("http://crm-dash/other-accounts");
+
+        String title = driver.getTitle();
+        Reporter.log("Utilizatorul a navigat cu succes la pagina - " + title);
+
+        Helpers.waitForSeconds(3);
+
+        WebElement select = driver.findElement(By.name("other-accounts-list_length"));
+        Select rows = new Select(select);
+        rows.selectByIndex(0);
+
+        Helpers.waitForSeconds(3);
+
+        driver.findElement(By.cssSelector(".fa-table")).click();
+
+        Helpers.waitForSeconds(3);
+
+        driver.findElement(By.id("setting-name")).sendKeys("AutoTableSetting");
+
+        Helpers.waitForSeconds(3);
+
+        SettingsHelper settingsHelper = new SettingsHelper(driver);
+
+        Helpers.waitForSeconds(3);
+        // Selectează multiple valori
+        String[] valuesToSelect = { "Warming Cost", "Monthly Fees", "Revenue", "Profit",
+                "Spend", "Created At",
+                "Other Fees" };
+        settingsHelper.selectMultipleValuesByValue(valuesToSelect);
+
+        // Apasă pe butonul de navigare
+        settingsHelper.clickNavigationButton("fa fa-arrow-circle-right");
+
+        // Mută elementele
+        settingsHelper.selectMultipleValuesByValue(new String[] { "Ban Reason" });
+        settingsHelper.moveElements("fa fa-arrow-circle-up", 20);
+
+        driver.findElement(By.cssSelector(".btn[data-wizard='next']")).click();
+
+        Helpers.waitForSeconds(3);
+
+        wait.until(ExpectedConditions.elementToBeClickable(By.id("apply-swap-list-settings"))).click();
+
+        Helpers.waitForSeconds(3);
+        Reporter.log("A fost creat cu succes noua setare cu coloanele:" + "\n");
+
+        Helpers.waitForSeconds(3);
+        List<WebElement> headers = driver.findElements(By.cssSelector(
+                ".dataTables_scrollHeadInner > table:nth-child(1) > thead:nth-child(1) > tr:nth-child(1) > th"));
+        List<WebElement> firstRow = driver
+                .findElements(By.cssSelector("#other-accounts-list tbody tr:first-child td"));
+
+        for (int i = 0; i < headers.size(); i++) {
+            // Scroll până la elementul curent din header
+            ((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView(true);", headers.get(i));
+
+            String header = headers.get(i).getText().trim();
+            String content = (i < firstRow.size()) ? firstRow.get(i).getText().trim() : "";
+
+            if (!header.isEmpty()) {
+                Reporter.log(header + " -> " + content);
+            } else {
+                Reporter.log("Header is empty for index ");
+            }
+
+        }
+    }
+}
