2489284fc6de7908b06a226a51d563aa3632c303
Cristea3051 <cristeavictor4@gmail.com>
2025-03-17 12:04:03 +0200

    dasdsadsa

--- src/test/java/com/crm/GoogleAccounts/DeleteTableSettingsOtherGoogleTest.java
+++ src/test/java/com/crm/GoogleAccounts/DeleteTableSettingsOtherGoogleTest.java
@@ -1,12 +1,9 @@
 package com.crm.GoogleAccounts;
 
-import java.time.Duration;
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
-
-import com.resources.configfiles.SettingsHelper;
+import com.Base.BaseTest;
+import com.resources.CredentialsProvider;
+import com.resources.Helpers;
+import com.utilities.Login;
 import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebElement;
@@ -15,12 +12,14 @@ import org.testng.Reporter;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
-import com.Base.BaseTest;
-import com.resources.CredentialsProvider;
-import com.resources.Helpers;
-import com.utilities.Login;
+import java.time.Duration;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
-public class CreateTableSettingsOtherGoogleTest extends BaseTest {
+public class DeleteTableSettingsOtherGoogleTest extends BaseTest {
+
     private Login login;
 
     @BeforeMethod
@@ -41,7 +40,6 @@ public class CreateTableSettingsOtherGoogleTest extends BaseTest {
         driver.get("http://crm-dash/google-accounts-v2");
         Helpers.waitForSeconds(3);
 
-        // Capturăm și printăm lista inițială
         Reporter.log("Lista completă a coloanelor înainte de modificări:");
         List<String> beforeHeaders = printTableHeaders();
         for (String header : beforeHeaders) {
@@ -49,52 +47,25 @@ public class CreateTableSettingsOtherGoogleTest extends BaseTest {
         }
         Reporter.log("Numărul total de coloane afișate: " + beforeHeaders.size());
 
-        // Aplicăm modificările
         driver.findElement(By.cssSelector("button.tw-mr-1:nth-child(5)")).click();
-        Helpers.waitForSeconds(3);
-
-        driver.findElement(By.id("setting-name")).sendKeys("AutoTableSetting");
-        Helpers.waitForSeconds(3);
-
-        SettingsHelper settingsHelper = new SettingsHelper(driver);
-        Helpers.waitForSeconds(3);
-
-        String[] valuesToSelect = { "Domains", "Mb Comments", "Farmer Comments", "Backup Code",
-                "Source Delivery Date", "Proxy Type", "License", "Backup Code", "Created At", "Sync From Date" };
-        settingsHelper.selectMultipleValuesByValue(valuesToSelect);
-
-        settingsHelper.clickNavigationButton("fa fa-arrow-circle-right");
-
-        settingsHelper.selectMultipleValuesByValue(new String[] { "Under Review Reason", "Under Review Date Time" });
-        settingsHelper.moveElements("fa fa-arrow-circle-up", 10);
         Helpers.waitForSeconds(3);
 
         driver.findElement(By.id("save-modal-swap-list")).click();
         Helpers.waitForSeconds(3);
         Reporter.log("A trecut pe next wizard");
 
-        driver.findElement(By.id("apply-swap-list-settings")).click();
+        driver.findElement(By.cssSelector("i.fa-trash")).click();
         Helpers.waitForSeconds(3);
+        driver.findElement(By.cssSelector("button.swal2-confirm")).click();
+        Helpers.waitForSeconds(3);
+        driver.findElement(By.id("apply-swap-list-settings")).click();
 
-        // Capturăm lista finală
         Reporter.log("Lista coloanelor după modificări:");
         List<String> afterHeaders = printTableHeaders();
         for (String header : afterHeaders) {
             Reporter.log(header);
         }
         Reporter.log("Numărul total de coloane afișate: " + afterHeaders.size());
-
-        // Concatenăm și comparăm listele
-        Reporter.log("Comparație înainte și după aplicarea setărilor:");
-        for (String beforeHeader : beforeHeaders) {
-            String result;
-            if (afterHeaders.contains(beforeHeader)) {
-                result = beforeHeader + " => " + beforeHeader;
-            } else {
-                result = beforeHeader + " => \"This column is hidden\"";
-            }
-            Reporter.log(result);
-        }
     }
 
     private List<String> printTableHeaders() {
