91cbc9a29ff08474ae7edcbfba68b843529cb81b
Cristea3051 <cristeavictor4@gmail.com>
2025-04-15 16:39:13 +0300

    dsadasdsad

--- src/test/java/com/crm/GoogleAccounts/CreateGoogleAccountTest.java
+++ src/test/java/com/crm/GoogleAccounts/CreateGoogleAccountTest.java
@@ -2,29 +2,28 @@ package com.crm.GoogleAccounts;
 
 import com.Base.BaseTest;
 import com.crm.GoogleAccounts.pages.GoogleAccountsPage;
-import com.crm.GoogleAccounts.pages.LoginPage;
 import com.resources.CredentialsProvider;
 import org.testng.Assert;
 import org.testng.Reporter;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
-
+import com.utilities.Login;
 public class CreateGoogleAccountTest extends BaseTest {
-        private LoginPage loginPage;
+        private Login login;
         private GoogleAccountsPage accountsPage;
 
         @BeforeMethod
         @Override
         public void setUp() {
                 super.setUp();
-                loginPage = new LoginPage(driver);
+                login = new Login(driver);
                 accountsPage = new GoogleAccountsPage(driver);
         }
 
         @Test(dataProvider = "GlobalCred", dataProviderClass = CredentialsProvider.class)
         public void signIn(String username, String password) {
-                loginPage.performLogin(username, password);
-                loginPage.closeDebugBar();
+                login.performLogin(username, password);
+                login.closeDebugBar();
                 accountsPage.navigateTo();
                 accountsPage.clickCreateButton();
                 accountsPage.fillCreateForm();
--- src/test/java/com/crm/GoogleAccounts/pages/GoogleAccountsPage.java
+++ src/test/java/com/crm/GoogleAccounts/pages/GoogleAccountsPage.java
@@ -35,7 +35,6 @@ public class GoogleAccountsPage {
     private By accountIdField = By.cssSelector("input.form-control.js-maxlength[name='account_id'][data-modal-field-id='create_account_id']");
     private By saveButton = By.cssSelector("button#create-google-accounts-button");
     private By editSaveButton = By.id("edit-google-accounts-button");
-    // Adaugă locatori pentru toate câmpurile din formular
 
     public GoogleAccountsPage(WebDriver driver) {
         this.driver = driver;
@@ -44,45 +43,55 @@ public class GoogleAccountsPage {
 
     public void navigateTo() {
         driver.get("http://crm-dash/google-accounts");
+        // Așteaptă ca pagina să fie încărcată complet
+        wait.until(ExpectedConditions.urlContains("google-accounts"));
         Reporter.log("Navigat la pagina: " + driver.getTitle());
     }
 
     public void selectRows(int index) {
         WebElement select = wait.until(ExpectedConditions.visibilityOfElementLocated(rowsSelect));
+        wait.until(ExpectedConditions.elementToBeClickable(select)); // Așteaptă să fie clicabil
         new Select(select).selectByIndex(index);
     }
 
     public void clickCreateButton() {
-        wait.until(ExpectedConditions.elementToBeClickable(createButton)).click();
+        WebElement button = wait.until(ExpectedConditions.visibilityOfElementLocated(createButton));
+        wait.until(ExpectedConditions.elementToBeClickable(button)).click();
     }
-
     public void fillCreateForm() {
         enterText(accountNameField, "GoogleAccountNameTestJava");
         enterText(accountIdField, "1234567890");
         selectDropdownOption(By.cssSelector("select.form-control.js-maxlength[name='status'][data-modal-field-id='create_status']"), 2);
-        // Adaugă restul câmpurilor din CreateGoogleAccount.fillFormFields
         clickAndSelectRandomDay(By.cssSelector("input.form-control.js-maxlength[name='id_verification'][data-modal-field-id='create_id_verification']"));
-        driver.findElement(saveButton).click();
+//      NU treb de uitat de adaugat alte elemente
+        WebElement saveBtn = wait.until(ExpectedConditions.visibilityOfElementLocated(saveButton));
+        wait.until(ExpectedConditions.elementToBeClickable(saveBtn)).click();
     }
 
     public void fillEditForm() {
-        enterText(By.cssSelector("input.form-control.js-maxlength[data-modal-field-id='edit_name'][maxlength='100']"), "GoogleAccountUpdatedTestJava");
-        enterText(By.cssSelector("input.form-control.js-maxlength[name='account_id'][data-modal-field-id='edit_account_id']"), "2345678901");
-        // Adaugă restul câmpurilor din UpdateGoogleAccount.fillFormFields
-        driver.findElement(editSaveButton).click();
+        By editNameField = By.cssSelector("input.form-control.js-maxlength[data-modal-field-id='edit_name'][maxlength='100']");
+        By editAccountIdField = By.cssSelector("input.form-control.js-maxlength[name='account_id'][data-modal-field-id='edit_account_id']");
+        enterText(editNameField, "GoogleAccountUpdatedTestJava");
+        enterText(editAccountIdField, "2345678901");
+        // Așteaptă ca editSaveButton să fie vizibil și clicabil
+        WebElement editSaveBtn = wait.until(ExpectedConditions.visibilityOfElementLocated(editSaveButton));
+        wait.until(ExpectedConditions.elementToBeClickable(editSaveBtn)).click();
     }
 
     public boolean searchAndVerify(String keyword) {
         enterText(searchInput, keyword);
+        // Așteaptă ca tabelul să fie vizibil
+        wait.until(ExpectedConditions.presenceOfAllElementsLocatedBy(tableHeaders));
         List<WebElement> headers = driver.findElements(tableHeaders);
         List<WebElement> firstRow = driver.findElements(firstRowCells);
         boolean found = false;
 
         for (int i = 0; i < headers.size(); i++) {
-            ((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView(true);", headers.get(i));
-            String header = headers.get(i).getText().trim();
+            WebElement header = wait.until(ExpectedConditions.visibilityOf(headers.get(i)));
+            ((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView(true);", header);
+            String headerText = header.getText().trim();
             String content = (i < firstRow.size()) ? firstRow.get(i).getText().trim() : "";
-            Reporter.log(header + " -> " + content);
+            Reporter.log(headerText + " -> " + content);
 
             if (content.contains(keyword)) {
                 found = true;
@@ -92,9 +101,11 @@ public class GoogleAccountsPage {
     }
 
     public void exportCsv() {
-        wait.until(ExpectedConditions.elementToBeClickable(exportCsvButton)).click();
+        WebElement exportBtn = wait.until(ExpectedConditions.visibilityOfElementLocated(exportCsvButton));
+        wait.until(ExpectedConditions.elementToBeClickable(exportBtn)).click();
         try {
-            wait.until(ExpectedConditions.elementToBeClickable(confirmExportButton)).click();
+            WebElement confirmBtn = wait.until(ExpectedConditions.visibilityOfElementLocated(confirmExportButton));
+            wait.until(ExpectedConditions.elementToBeClickable(confirmBtn)).click();
             Reporter.log("Fișier CSV descărcat cu succes");
         } catch (Exception e) {
             Reporter.log("Eroare la descărcarea fișierului CSV: " + e.getMessage());
@@ -102,44 +113,62 @@ public class GoogleAccountsPage {
     }
 
     public void deleteAccount(String keyword) {
-        enterText(By.cssSelector("input.form-control[type='search']"), keyword);
-        wait.until(ExpectedConditions.elementToBeClickable(rowClick)).click();
-        wait.until(ExpectedConditions.elementToBeClickable(deleteButton)).click();
-        wait.until(ExpectedConditions.elementToBeClickable(confirmDeleteButton)).click();
+        By searchField = By.cssSelector("input.form-control[type='search']");
+        enterText(searchField, keyword);
+        WebElement row = wait.until(ExpectedConditions.visibilityOfElementLocated(rowClick));
+        wait.until(ExpectedConditions.elementToBeClickable(row)).click();
+        WebElement deleteBtn = wait.until(ExpectedConditions.visibilityOfElementLocated(deleteButton));
+        wait.until(ExpectedConditions.elementToBeClickable(deleteBtn)).click();
+        WebElement confirmBtn = wait.until(ExpectedConditions.visibilityOfElementLocated(confirmDeleteButton));
+        wait.until(ExpectedConditions.elementToBeClickable(confirmBtn)).click();
         Reporter.log("Cont șters cu succes: " + keyword);
     }
 
     public void editAccount(String keyword) {
-        enterText(By.cssSelector("input.form-control[type='search']"), keyword);
-        wait.until(ExpectedConditions.elementToBeClickable(rowClick)).click();
-        wait.until(ExpectedConditions.elementToBeClickable(editButton)).click();
+        By searchField = By.cssSelector("input.form-control[type='search']");
+        enterText(searchField, keyword);
+        WebElement row = wait.until(ExpectedConditions.visibilityOfElementLocated(rowClick));
+        wait.until(ExpectedConditions.elementToBeClickable(row)).click();
+        WebElement editBtn = wait.until(ExpectedConditions.visibilityOfElementLocated(editButton));
+        wait.until(ExpectedConditions.elementToBeClickable(editBtn)).click();
     }
 
     private void enterText(By locator, String text) {
-        WebElement element = driver.findElement(locator);
+        WebElement element = wait.until(ExpectedConditions.visibilityOfElementLocated(locator));
+        wait.until(ExpectedConditions.elementToBeClickable(element));
         element.clear();
         element.sendKeys(text);
     }
 
     private void selectDropdownOption(By locator, int index) {
-        WebElement selectElement = driver.findElement(locator);
+        WebElement selectElement = wait.until(ExpectedConditions.visibilityOfElementLocated(locator));
+        wait.until(ExpectedConditions.elementToBeClickable(selectElement));
         new Select(selectElement).selectByIndex(index);
     }
 
     private void clickAndSelectRandomDay(By locator) {
-        driver.findElement(locator).click();
-        WebElement activeCalendar = driver.findElement(By.cssSelector(".flatpickr-calendar.open"));
+        WebElement dateInput = wait.until(ExpectedConditions.visibilityOfElementLocated(locator));
+        wait.until(ExpectedConditions.elementToBeClickable(dateInput)).click();
+        // Așteaptă ca flatpickr să fie vizibil
+        WebElement activeCalendar = wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector(".flatpickr-calendar.open")));
         List<WebElement> days = activeCalendar.findElements(By.cssSelector(".flatpickr-day:not(.prevMonthDay):not(.nextMonthDay)"));
         if (!days.isEmpty()) {
             Random random = new Random();
-            days.get(random.nextInt(days.size())).click();
+            WebElement day = wait.until(ExpectedConditions.elementToBeClickable(days.get(random.nextInt(days.size()))));
+            day.click();
         } else {
             Reporter.log("Nu există zile disponibile în calendar.");
         }
     }
 
-    // Reutilizează handleAutocomplete din CreateGoogleAccount
     public void handleAutocomplete(By locator, String inputText, By autocompleteListLocator) {
-        // Copiază metoda handleAutocomplete din CreateGoogleAccount.java
+        enterText(locator, inputText);
+        // Așteaptă lista de autocomplete
+        WebElement autocompleteList = wait.until(ExpectedConditions.visibilityOfElementLocated(autocompleteListLocator));
+        List<WebElement> options = autocompleteList.findElements(By.tagName("li"));
+        if (!options.isEmpty()) {
+            WebElement option = wait.until(ExpectedConditions.elementToBeClickable(options.get(0)));
+            option.click();
+        }
     }
 }
